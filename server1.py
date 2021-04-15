52949214f2b7fa85912c3021817a049066c3b145
Prajapati omkar <72141037+prajapatiomkar@users.noreply.github.com>
2021-04-15 07:59:52 +0530

    server1

---
+++ server1.py
@@ -0,0 +1,18 @@
+# Server_code
+
+import socket
+s = socket.socket()
+print('Socket Created')
+
+s.bind(('localhost', 9999))
+
+s.listen(3)
+print('Waiting for connection')
+
+while True:
+    c, addr = s.accept()
+    name = c.recv(1024).decode()
+    print("Connected with", addr, name)
+
+    c.send(bytes('Welcome', 'utf-8'))
+    c.close()
